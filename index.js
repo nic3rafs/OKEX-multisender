0b4ab5922be27d8dcf1159b7461c1cfe1ebe75fa
partachu.eth <43925652+partachu@users.noreply.github.com>
2023-02-06 00:28:07 +0100

    Added random amount and random time to sleep

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ import fs from "fs";
 dotenv.config();
 
 //CONFIG
-const coin = "BNB";
-const network = "BSC";
-const amount = 0.01;
-const networkFee = 0.002
+const coin = "ETH"; // ETH for ETH
+const network = "ETH"; // ETH for ETH, BSC for BinanceSmartChain
+const networkFee = 0.00096;
+
+const minTimeSleep = 10000; // time in milliseconds
+const maxTimeSleep = 15000;
+
+const minAmount = 0.01;
+const maxAmount = 0.02;
+const digitsAfterPeriod = 4; // How much digits to have after period in send amount
+//================================================================
 
 const ccxtConfig = {
   enableRateLimit: true,
@@ -15,7 +22,6 @@ const ccxtConfig = {
   secret: process.env.OKEX_SECRET_KEY,
   password: process.env.OK_ACCESS_PASSPHRASE,
 };
-
 
 const readFileAsArray = async (fileName) => {
   try {
@@ -27,12 +33,25 @@ const readFileAsArray = async (fileName) => {
     return null;
   }
 };
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function randomAmount(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(digitsAfterPeriod);
+}
+function randomSleep(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const main = async () => {
   const okex = new ccxt.okex(ccxtConfig);
+
   try {
     const addresses = await readFileAsArray("addresses.txt");
     for (const address of addresses) {
+      const amount = randomAmount(minAmount, maxAmount);
       try {
         const withdrawResponse = await okex.withdraw(coin, amount, address, {
           fee: networkFee,
@@ -41,11 +60,14 @@ const main = async () => {
         });
 
         if (withdrawResponse.info.wdId) {
-          console.log(`Succesfull withdraw for (${address})`);
+          console.log(`Succesfull withdraw ${amount}${coin} for (${address})`);
         }
       } catch (error) {
         console.log(`[Error] with (${address}) - ${error}`);
       }
+      const sleepTime = randomSleep(minTimeSleep, maxTimeSleep);
+      console.log(`Sleeping for ${sleepTime} ms `);
+      await sleep(sleepTime);
     }
   } catch (error) {
     console.log(error);
